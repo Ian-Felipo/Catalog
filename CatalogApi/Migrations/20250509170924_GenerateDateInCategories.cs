9ac7a08f28e87ba1f0751aeb5bde3920aa7149af
Ian Felipo <ianfelippo2@gmail.com>
2025-05-09 15:08:13 -0300

    migrations 'GenerateDateInProducts'

--- CatalogApi/Migrations/20250509170924_GenerateDateInCategories.cs
+++ CatalogApi/Migrations/20250509170924_GenerateDateInCategories.cs
@@ -10,13 +10,17 @@ namespace CatalogApi.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("INSERT INTO Categories(Name, ImageUrl) Values('Drinks', 'Drinks.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name, ImageUrl) Values('Desserts', 'Desserts.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name, ImageUrl) Values('Meats', 'Meats.jpg')");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("DELETE FROM Categories WHERE Name = 'Drinks'");
+            migrationBuilder.Sql("DELETE FROM Categories WHERE Name = 'Desserts'");
+            migrationBuilder.Sql("DELETE FROM Categories WHERE Name = 'Meats'");
         }
     }
 }
---
+++ CatalogApi/Migrations/20250509172923_GenerateDateInProducts.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class GenerateDateInProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql
+            (
+                @"INSERT INTO Products(Name, Description, Price, ImageUrl, Stock, RegistrationDate, CategoryId) 
+                VALUES(""Beer"", "" "", 10.22, ""Beer.jpg"", 5.45, now(), 1)"
+            );
+            migrationBuilder.Sql
+            (
+                @"INSERT INTO Products(Name, Description, Price, ImageUrl, Stock, RegistrationDate, CategoryId) 
+                VALUES(""Cake"", "" "", 27.98, ""Cake.jpg"", 60.00, now(), 2)"
+            );
+            migrationBuilder.Sql
+            (
+                @"INSERT INTO Products(Name, Description, Price, ImageUrl, Stock, RegistrationDate, CategoryId) 
+                VALUES(""Chicken"", "" "", 15.68, ""Chicken.jpg"", 50.78, now(), 3)"
+            );        
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categories WHERE Name = 'Beer'");
+            migrationBuilder.Sql("DELETE FROM Categories WHERE Name = 'Cake'");
+            migrationBuilder.Sql("DELETE FROM Categories WHERE Name = 'Chicken'");
+        }
+    }
+}
