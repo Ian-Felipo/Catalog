3cf32a04f23a5b399ac399d786b3e732bc25e0f6
Ian <ianfelippo2@gmail.com>
2025-05-08 21:55:41 -0300

    Adding data annotations to Product model

--- CatalogApi/Models/Product.cs
+++ CatalogApi/Models/Product.cs
@@ -1,11 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace CatalogApi.Models;
 
+[Table("Products")]
 public class Product
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
+    [StringLength(100)]
     public string? Name { get; set; }
+    [Required]
+    [StringLength(300)]
     public string? Description { get; set; }
+    [Required]
+    [Column(TypeName = "decimal(10,2)")]
     public decimal Price { get; set; }
+    [Required]
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
     public float Stock { get; set; }
     public DateTime RegistrationDate { get; set; }
