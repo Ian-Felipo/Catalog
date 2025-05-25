afca32b6337e49bc19f205c6886da8ca0ae9d20d
Ian <ianfelippo2@gmail.com>
2025-05-25 16:23:17 -0300

    GetProducts

--- CatalogApi/Interfaces/IProductRepository.cs
+++ CatalogApi/Interfaces/IProductRepository.cs
@@ -1,7 +1,9 @@
 using CatalogApi.Models;
+using CatalogApi.Parameters;
 
 namespace CatalogApi.Interfaces;
 
 public interface IProductRepository : IRepository<Product>
 {
+    IEnumerable<Product> GetProducts(ProductsParameters productsParameters);
 }
--- CatalogApi/Repositories/ProductRepository.cs
+++ CatalogApi/Repositories/ProductRepository.cs
@@ -1,6 +1,7 @@
 using CatalogApi.Data;
 using CatalogApi.Interfaces;
 using CatalogApi.Models;
+using CatalogApi.Parameters;
 using Microsoft.EntityFrameworkCore;
 
 namespace CatalogApi.Repositories;
@@ -9,5 +10,10 @@ public class ProductRepository : Repository<Product>, IProductRepository
 {
     public ProductRepository(CatalogApiDbContext catalogApiDbContext) : base(catalogApiDbContext)
     {
+    }
+
+    public IEnumerable<Product> GetProducts(ProductsParameters productsParameters)
+    {
+        return GetAll().Skip((productsParameters.PageNumber - 1) * productsParameters.PageSize).Take(productsParameters.PageSize).ToList();
     }
 }
