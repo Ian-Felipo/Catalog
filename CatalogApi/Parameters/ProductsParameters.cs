32a2272bf011c637e47f9d9b9176b35325670c01
Ian <ianfelippo2@gmail.com>
2025-05-25 16:13:55 -0300

    ProductsParameters

---
+++ CatalogApi/Parameters/ProductsParameters.cs
@@ -0,0 +1,19 @@
+namespace CatalogApi.Parameters;
+
+public class ProductsParameters
+{
+    private const int MaxPageSize = 50;
+    public int PageNumber { get; set; } = 1;
+    private int _PageSize = 10;
+    public int PageSize
+    {
+        get
+        {
+            return _PageSize;
+        }
+        set
+        {
+            _PageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        } 
+    }
+}
