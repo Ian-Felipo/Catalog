e4fed3e6fee538c5bfd5752fc928fabba8b9a9b2
Ian Felipo <ianfelippo2@gmail.com>
2025-05-27 14:43:09 -0300

    Configure JwtBearer

--- CatalogApi/Program.cs
+++ CatalogApi/Program.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Text.Json.Serialization;
 using CatalogApi.AutoMappers;
 using CatalogApi.Data;
@@ -5,27 +6,29 @@ using CatalogApi.Extensions;
 using CatalogApi.Filters;
 using CatalogApi.Interfaces;
 using CatalogApi.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles); // ********************
 builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
 builder.Services.AddScoped<IProductRepository,ProductRepository>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddAutoMapper(typeof(CategoryMapperProfile), typeof(ProductMapperProfile));
-builder.Services.AddAuthentication("Bearer").AddBearerToken();
 builder.Services.AddAuthorization();
 
 builder.Services.AddControllers(options =>
     {
         options.Filters.Add(typeof(ExceptionFilter));
     }
-).AddNewtonsoftJson();
+)
+.AddNewtonsoftJson()
+.AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 builder.Services.AddDbContext<CatalogApiDbContext>(options =>
     {
@@ -33,6 +36,29 @@ builder.Services.AddDbContext<CatalogApiDbContext>(options =>
         options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
     }
 );
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+).AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ClockSkew = TimeSpan.Zero,
+            ValidAudience = builder.Configuration["JWT:ValidAudience"],
+            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecretKey"]))
+        };
+    }
+).AddBearerToken();
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<CatalogApiDbContext>().AddDefaultTokenProviders();
 
@@ -48,5 +74,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.MapControllers();
 app.Run();
