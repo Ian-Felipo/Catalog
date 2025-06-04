9546b337695d3b493e1e548ed6704191f1876719
Ian Felipo <ianfelippo2@gmail.com>
2025-06-04 13:56:10 -0300

    Add Register and Login DTOs

---
+++ CatalogApi/DTOs/LoginResponse.cs
@@ -0,0 +1,7 @@
+namespace CatalogApi.DTOs;
+
+public class LoginResponse
+{
+    public string? AccessToken { get; set; }
+    public string? RefreshToken { get; set; }
+}
---
+++ CatalogApi/DTOs/RegisterResponse.cs
@@ -0,0 +1,7 @@
+namespace CatalogApi.DTOs;
+
+public class RegisterResponse
+{
+    public string? Status { get; set; }
+    public string? Message { get; set; }
+}
